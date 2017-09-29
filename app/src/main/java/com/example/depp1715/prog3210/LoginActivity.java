c4c8a3bfc83695d8d82efaa6c5d73c4cf83c2110
Doug Epp <Depp1715@conestogac.on.ca>
2017-09-29 09:53:49 -0400

    Added navigation between login activity and main activity

--- app/src/main/java/com/example/depp1715/prog3210/LoginActivity.java
+++ app/src/main/java/com/example/depp1715/prog3210/LoginActivity.java
@@ -1,13 +1,34 @@
 package com.example.depp1715.prog3210;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText txtUserName;
+    private EditText txtPassword;
+    private Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        txtUserName = (EditText) findViewById(R.id.txtUserName);
+        txtPassword = (EditText) findViewById(R.id.txtPassword);
+        btnSubmit = (Button) findViewById(R.id.btnSubmit);
+
+        btnSubmit.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent mainActivityIntent = new Intent(this, MainActivity.class);
+
+        startActivity(mainActivityIntent);
+        this.finish();
     }
 }
